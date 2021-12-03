f6bc9f485c5ce3bd303b8ad925dd9856cc3f5754
Rakib013 <mdrakib32661@gmail.com>
2021-12-03 12:21:59 +0600

    Blog and possibility page added

--- src/containers/Blog.jsx
+++ src/containers/Blog.jsx
@@ -1,11 +1,90 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Article from '../components/Article';
+import blog01 from "../assets/blog01.png";
+import blog02 from "../assets/blog02.png";
+import blog03 from "../assets/blog03.png";
+import blog04 from "../assets/blog04.png";
+import blog05 from "../assets/blog05.png";
+
 
 function Blog() {
     return (
-        <div>
-            
-        </div>
+        <BLOG className="section__padding">
+            <Headings>
+                <h1 className="gradient__text">A lot is happening we are blogging about it.</h1>
+            </Headings>
+            <Container>
+                <ContainerA>
+                    <Article imgUrl={blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                </ContainerA>
+                <ContainerB>
+                    <Article imgUrl={blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                    <Article imgUrl={blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+                </ContainerB>
+            </Container>
+        </BLOG>
     )
 }
 
 export default Blog
+
+
+const BLOG = styled.div``
+
+const Headings = styled.div`
+    width: 100%;
+    text-align: left;
+    margin-bottom: 5rem;
+
+    &>h1{
+        font-family: var(--font-family);
+        font-size: 62px;
+        line-height: 75px;
+        font-weight: 800;
+        letter-spacing: -0.04rem;
+
+        @media screen and (max-width: 990px){
+            font-size: 46px;
+            line-height: 52px;
+        }
+
+        @media screen and (max-width: 550px){
+            font-size: 34px;
+            line-height: 42px;
+        }
+    }
+`
+
+const Container = styled.div`
+    display: flex;
+
+    @media screen and (max-width: 990px){
+        flex-direction: column-reverse;
+    }
+`
+
+const ContainerA = styled.div`
+    flex: 0.75;
+    margin-right: 2rem;
+
+    @media screen and (max-width: 990px){
+        margin: 2rem 0;
+        width: 100%;
+        height: 250px;
+    }
+`
+
+const ContainerB = styled.div`
+    flex: 1;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 2rem;
+
+    @media screen and (max-width: 700px){
+        grid-template-columns: repeat(1, 1fr);
+        width: 100%;
+    }
+`
--- src/containers/Possibility.jsx
+++ src/containers/Possibility.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import possibilityIMG from "../assets/possibility.png"
 
 function Possibility() {
     return (
-        <div>
-            
-        </div>
+        <POssibility className="section__padding" id="posibility">
+            <PImage>
+                <img src={possibilityIMG} alt="" />
+            </PImage>
+            <Content>
+                    <h4>Request Early Access to Get Started</h4>
+                    <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
+                    <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+                    <h4>Request Early Access to Get Started</h4>
+            </Content>
+        </POssibility>
     )
 }
 
-export default Possibility
+export default Possibility;
+
+
+const POssibility = styled.div`
+    display: flex;
+    @media screen and (max-width: 950px){
+        flex-direction: column;
+    } 
+`
+
+const PImage = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-right: 2rem;
+
+    &>img{
+        width: 100%;
+        height: 100%;
+    }
+
+    @media screen and (max-width: 950px){
+        margin: 1rem 0;
+    } 
+`
+
+const Content = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-start;
+
+    &>h4{
+        font-family: var(--font-family);
+        font-weight: 500;
+        color: #71E5FF;
+        font-size: 16px;
+        line-height: 30px;
+    }
+
+    &>h4:last-child{
+        color: #FF8A71;
+    }
+
+    &>h1{
+        font-family: var(--font-family);
+        font-weight: 800;
+        font-size: 34px;
+        line-height: 45px;
+    }
+
+    &>p{
+        font-family: var(--font-family);
+        font-size: 16px;
+        line-height: 30px;
+        margin-bottom: 2rem;
+        color: var(--color-text);
+    }
+`
